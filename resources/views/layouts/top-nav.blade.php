513a41f100089a2820cf1f2a3e929657d6dfe912
Soponloe <sovannsoponloe@gmail.com>
2024-08-09 09:30:21 +0700

    made changes to nav bars

--- resources/views/layouts/top-nav.blade.php
+++ resources/views/layouts/top-nav.blade.php
@@ -1,6 +1,6 @@
 <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
     <!-- Navbar Brand-->
-    <a class="navbar-brand ps-3" href="#" style="font-family: 'Moulpali', sans-serif;">អប្សរាចម្លាក់ខ្មែរ</a>
+    <a class="navbar-brand ps-3" href="{{ route('dashboard') }}" style="font-family: 'Moulpali', sans-serif;">អប្សរាចម្លាក់ខ្មែរ</a>
     <!-- Sidebar Toggle-->
     <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i
             class="fas fa-bars"></i></button>
