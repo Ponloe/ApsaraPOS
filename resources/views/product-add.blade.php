b18c3171ae793023a763590f71f4233b6b1c4619
Soponloe <sovannsoponloe@gmail.com>
2024-08-09 09:23:40 +0700

    made changes to add products

--- resources/views/product-add.blade.php
+++ resources/views/product-add.blade.php
@@ -12,7 +12,14 @@
           <div class="col-md-12 mb-3">
             <label for="selectCategory">Select Category *</label>
             <select class="form-control" id="selectCategory">
-              <option>Apsara</option>
+              <option>កំពូល</option>
+            </select>
+          </div>
+          <div class="col-md-12 mb-3">
+            <label for="selectCategory">Select Warehouse *</label>
+            <select class="form-control" id="selectCategory">
+              <option>ឃ្លាំង ១</option>
+              <option>ឃ្លាំង ២</option>
             </select>
           </div>
           <div class="col-md-12 mb-3">
@@ -20,10 +27,14 @@
             <input type="text" class="form-control" id="productName" placeholder="Enter product name">
           </div>
           <div class="col-md-12 mb-3">
+            <label for="productName">Code *</label>
+            <input type="text" class="form-control" id="Code" placeholder="Enter Code ">
+          </div>
+          <!-- <div class="col-md-12 mb-3">
             <label for="productDescription">Description</label>
             <textarea class="form-control" id="productDescription" rows="3"
               placeholder="Enter product description"></textarea>
-          </div>
+          </div> -->
           <div class="row">
             <div class="col-md-4 mb-3">
               <label for="productPrice">Price *</label>
@@ -52,5 +63,11 @@
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </main>
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Khmer&family=Moulpali&family=Noto+Sans+Khmer:wght@100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
 
+    body {
+        font-family: 'Khmer', 'Moulpali', 'Noto Sans Khmer', 'Poppins', sans-serif;
+    }
+</style>
 @endsection
